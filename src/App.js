8eeccb40193be944c1fcd8b4237425032c6183d8
Neel Patel <npate306@illinois.edu>
2018-10-06 00:41:51 -0500

    added contact information

--- src/App.js
+++ src/App.js
@@ -6,86 +6,26 @@ import firebase from './firebase.js';
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      currentItem: '',
-      username: '',
-      items: []
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+
   }
-  handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
-  handleSubmit(e) {
-    e.preventDefault();
-    const itemsRef = firebase.database().ref('items');
-    const item = {
-      title: this.state.currentItem,
-      user: this.state.username
-    }
-    itemsRef.push(item);
-    this.setState({
-      currentItem: '',
-      username: ''
-    });
-  }
-  componentDidMount() {
-    const itemsRef = firebase.database().ref('items');
-    itemsRef.on('value', (snapshot) => {
-      let items = snapshot.val();
-      let newState = [];
-      for (let item in items) {
-        newState.push({
-          id: item,
-          title: items[item].title,
-          user: items[item].user
-        });
-      }
-      this.setState({
-        items: newState
-      });
-    });
-  }
-  removeItem(itemId) {
-    const itemRef = firebase.database().ref(`/items/${itemId}`);
-    itemRef.remove();
-  }
+
   render() {
     return (
-      <div className='app'>
-        <header>
-            <div className="wrapper">
-              <h1>Fun Food Friends</h1>
 
-            </div>
-        </header>
-        <div className='container'>
-          <section className='add-item'>
-                <form onSubmit={this.handleSubmit}>
-                  <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
-                  <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
-                  <button>Add Item</button>
-                </form>
-          </section>
-          <section className='display-item'>
-              <div className="wrapper">
-                <ul>
-                  {this.state.items.map((item) => {
-                    return (
-                      <li key={item.id}>
-                        <h3>{item.title}</h3>
-                        <p>brought by: {item.user}
-                          <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
-                        </p>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
-          </section>
+      <div className='app'>
+        <h1 id="header1"> Scheduling </h1>
+
+        <div class="w3-container">
+        <h1 id="header1"> Our Locations and Numbers: </h1>
+        <div class="w3-panel w3-card" id="header1"> <p>3000 Biscayne Blvd Suite 210
+          Miami, FL 33137
+          305-576-5001 </p></div>
+          <div class="w3-panel w3-card-2" id="header1"><p>151 NW 11th St Suite 400
+          Homestead, FL 33030 786-414-1309</p></div>
+          <div class="w3-panel w3-card-4" id="header1"><p>1350 SW 4th St Homestead, FL 33030
+          786-414-1308</p></div>
+          <div class="w3-panel w3-card-4" id="header1"><p>975 Baptist Way Homestead, FL 33030
+          786-573-6879</p></div>
         </div>
       </div>
     );
