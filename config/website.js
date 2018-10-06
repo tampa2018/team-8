9c2118bc7583b32ef3d7ea5bf52e695bcd2852f2
Tony Faieta <tfaieta@gmail.com>
2018-10-06 09:49:47 -0400

    Updated website.js

--- config/website.js
+++ config/website.js
@@ -2,14 +2,14 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   siteTitle: 'Catalyst Miami', // Navigation and Site Title
   siteTitleAlt: 'Catalyst Miami Code For Good', // Alternative Site title for SEO
-  siteUrl: 'https://portfolio-emma.netlify.com', // Domain of your site. No trailing slash!
+  siteUrl: '', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
   siteDescription: 'Web App Code For Good',
-  author: 'LekoArts', // Author for schemaORGJSONLD
+  author: 'Team 8', // Author for schemaORGJSONLD
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@emma', // Twitter Username
-  ogSiteName: 'emma', // Facebook Site Name
+  userTwitter: '@CatalystMiami', // Twitter Username
+  ogSiteName: 'https://www.facebook.com/CatalystMiami/', // Facebook Site Name
   ogLanguage: 'en_US',
   googleAnalyticsID: 'UA-12345689-1',
   // Manifest and Progress color
