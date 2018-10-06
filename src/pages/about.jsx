5fed7fb87aeee3bbf457fb95d5356671713ca317
Tony Faieta <tfaieta@gmail.com>
2018-10-06 03:14:21 -0400

    Updated about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -8,15 +8,19 @@ const About = () => (
     <Helmet title={`About | ${config.siteTitle}`} />
     <Header>About</Header>
     <Container type="text">
-      <h1>Hi!</h1>
+      <h1 style={{textAlign: 'center'}}>Two decades ago, Catalyst Miami emerged as a critical node in the local 
+      network of nonprofit organizations.</h1>
       <p>
-        Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind
-        texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A
-        small river named Duden flows by their place and supplies it with the necessary regelialia. It is a
-        paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing
-        has no control about the blind texts it is an almost unorthographic life One day however a small line of blind
-        text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to
-        do so, because there were thousands.
+      <br/>
+      Founded by Daniella Levine Cava as the Human Services Coalition, our defining role is to identify and launch innovative community building strategies.
+
+      Our Vision
+
+      A just and equitable society in which all communities thrive.
+
+      Our Mission
+
+      To identify and collectively solve issues adversely affecting low-wealth communities throughout Miami-Dade County.
       </p>
     </Container>
   </Layout>
