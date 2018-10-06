5ceef9e23b9de0c76f672316f38d011d90aa7ee0
Neel Patel <npate306@illinois.edu>
2018-10-06 06:59:22 -0500

    added user stories

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import firebase from './firebase.js';
+import  './child.js';
 
 class App extends Component {
   constructor() {
@@ -13,22 +14,100 @@ class App extends Component {
     return (
 
       <div className='app'>
-        <h1 id="header1"> Scheduling </h1>
+        <h1 id="header1"> Experiences </h1>
 
         <div class="w3-container">
-        <h1 id="header1"> Our Locations and Numbers: </h1>
-        <div class="w3-panel w3-card" id="header1"> <p>3000 Biscayne Blvd Suite 210
-          Miami, FL 33137
-          305-576-5001 </p></div>
-          <div class="w3-panel w3-card-2" id="header1"><p>151 NW 11th St Suite 400
-          Homestead, FL 33030 786-414-1309</p></div>
-          <div class="w3-panel w3-card-4" id="header1"><p>1350 SW 4th St Homestead, FL 33030
-          786-414-1308</p></div>
-          <div class="w3-panel w3-card-4" id="header1"><p>975 Baptist Way Homestead, FL 33030
-          786-573-6879</p></div>
+        <div class="w3-panel w3-card" id="header1"> <h1>  ROBERT CLEMENT </h1>
+        <img src={"http://catalystmiami.org/wp-content/uploads/2015/06/story1.jpg"} /><p>
+I came to Catalyst Miami for help completing my 2013 taxes. During my initial visit, I mentioned that I had moved to Miami because I loved music and hoped to pursue my dream of being musician. After arriving in South Florida, I decided to return to school, and enrolled in Miami Dade College to earn an associate’s degree with a concentration in music. Unfortunately, my living situation was unstable. I soon found myself homeless and was forced to drop out of school.
+After living in a homeless shelter for a month and a half, I was able to secure employment and a room for rent, which allowed me to pay off my debts.
+Catalyst Miami Prosperity Wealth team first provided me with Free Tax Preparation services. From there, I committed to a budgeting session with a Catalyst Miami financial coach. My primary financial goal was to get caught up with my debts, which included owing money for my rent, car, car insurance and credit card. I also wanted to establish an emergency savings fund.
+Catalyst Miami’s Volunteer Income Tax Assistance preparation program was able to secure a tax refund of $1,146 for me, including an Earned Income Tax Credit of $299. Of course, this was great news. With my refund, I was able to catch up on some of the outstanding balances that stood between my goals and me.
+I continue to work with the financial coaching team at Catalyst Miami to improve my financial life. I’m now current on my rent, car payment, and auto insurance. My financial coach also helped me with my resume. I attained a second job, returned to school, and received my degree. I plan to use Catalyst Miami’s Ways 2 Wealth savings program to continue building my emergency savings, allocate savings towards my musical career, and enjoy life by traveling. My next step is to attain a job in the Miami area in music while pursuing a record deal.</p>
+<h3> Partners for financial security: </h3>
+<ul>
+	<li> Coalition on Human Needs </li>
+	<li> Collaborative Development Corporation </li>
+	<li> Collective Empowerment Group </li>
+	<li> Community Health Worker Coalition </li>
+	<li> Faith in Florida </li>
+	<li> Family Values at Work </li>
+	<li> Florida Prosperity Partnership </li>
+	<li> Florida Rights Restoration Coalition </li>
+	<li> Haitian American CDC </li>
+	<li> Half in 10 Campaign </li>
+	<li> Injury Free Coalition </li>
+	<li> KidCare Coalition </li>
+	<li> Miami Dade County PTA/PTSA </li>
+	<li> North East 2nd Avenue Partnership (NE2P) </li>
+	<li> Overtown Youth and Family Coalition </li>
+	<li> RAISE Florida </li>
+	<li> South Florida Community Development Coalition </li>
+	<li> Southern Christian Leadership Conference </li>
+	<li> War on Poverty FL </li>
+	<li> We Care of South Dade, Inc. </li>
+
+</ul>
+</div>
+<div class="w3-panel w3-card" id="header1"> <h1>  VANESSA NAVARRO </h1>
+<img src={"http://catalystmiami.org/wp-content/uploads/2015/06/story2.jpg"} /><p>
+Through my Public Allies Miami apprenticeship, I was able to look at my hometown through a new lens. Working as a community organizer in Miami allowed me to understand the issues relevant to each community within this metropolis and to consider different solutions for the problems we face. As I worked in these communities, I became frustrated with the fact that many people in Miami are dealing with difficult situations and are in need of much support. Through this program, I realized that one person’s struggle or injustice is everyone’s struggle. I am so pleased to continue working in Miami following my term with Public Allies Miami as HistoryMiami’s Folklife Specialist. My goal is to continue to bring Miamians together by celebrating our shared history and allowing people to learn more about their neighbors. I’m grateful to the Public Allies Miami program for giving me the opportunity to appreciate my community in a different way and strengthening my commitment to making Miami a better place for everyone.</p>
+<h3> Partners for community leadership: </h3>
+<ul>
+	<li> Baptist Health Foundation </li>
+	<li> Belafonte TACOLCY </li>
+	<li> Big Brothers Big Sisters </li>
+	<li> Camillus House </li>
+	<li> Carrfour Supportive Housing </li>
+	<li> Center for Independent Living of South Florida </li>
+	<li> Children's Movement of Florida </li>
+	<li> City of Miami </li>
+	<li> Collaborative Development Corporation </li>
+	<li> Collective Empowerment Group </li>
+	<li> Community Action Agency and Human Services </li>
+	<li> Concerned African Women, Inc. </li>
+	<li> Early Learning Coalition of Miami-Dade </li>
+	<li> Fairchild Tropical Botanical Gardens </li>
+	<li> Federation of Families for Children's Mental Health </li>
+	<li> Florida Immigrant Coalition (FLIC) </li>
+	<li> Florida International University </li>
+	<li> Gang Alternative </li>
+	<li> Girl Power Rocks </li>
+	<li> Haitian American CDC </li>
+	<li> Injury Free Coalition </li>
+	<li> Joseph H & Florence A. Roblee Foundation </li>
+	<li> Jungle Island </li>
+	<li> MDCPS Comprehensive Health Services </li>
+	<li> Miami Beach Botanical Gardens </li>
+	<li> Miami Children's Initiative </li>
+	<li> Miami Dade County </li>
+	<li> Miami Dade County PTA/PTSA </li>
+	<li> Miami Lighthouse for the Blind </li>
+	<li> National Congress of Black Women </li>
+	<li> National Parent Leadership Institute </li>
+</ul>
+ </div>
+<div class="w3-panel w3-card" id="header1"> <h1>  YANICK LANDESS </h1>
+<img src={"http://catalystmiami.org/wp-content/uploads/2015/06/story3.jpg"} /><p>
+PLTI was an exciting learning experience. I learned to interact with other members of the community--other stakeholders, parents, businesses, agencies, and with city, county, and government officials. From my training, I learned that elected officials are regular people just like me, and that I am capable of connecting with them. I know now that they are here to serve my needs, my children’s needs, and the needs of the community. PLTI helped me understand why it’s important to advocate, to be civically engaged, and to be a part of the change that we want to see. I was able to put my leadership skills to practice during the Tallahassee Days of Action trip and an advocacy trip to Washington D.C.
+
+There is nothing like knowledge and leadership skills to empower residents and to help them empower others. PLTI gave me that empowerment. Now I am paying it forward. I created a “neighbor circle” in my neighborhood to strengthen my community. I am also actively engaged in policy advocacy because of my participation in this program.</p>
+<h3> Partners for government: </h3>
+<ul>
+	<li> AFSCME 199 </li>
+	<li> City of Miami Gardens </li>
+	<li> Community Action Agency and Human Services </li>
+	<li> Department of Children and Family </li>
+	<li> Hialeah Housing Authority </li>
+	<li> Miami Dade County </li>
+	<li> Miami Dade Public Library System </li>
+</ul>
+</div>
+
         </div>
       </div>
     );
   }
 }
+
 export default App;
