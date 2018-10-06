57f8a1ec2c346c64f54a842940ab74e796b11ddb
Stephen Mylabathula <stephen@Stephens-MacBook-Pro-2.local>
2018-10-06 10:48:42 -0400

    fix merge

---
+++ .cache/sync-requires.js
@@ -0,0 +1,21 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---src-templates-project-jsx": preferDefault(require("/Users/stephen/Development/Projects/team-8/src/templates/project.jsx")),
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/stephen/Development/Projects/team-8/.cache/dev-404-page.js")),
+  "component---src-pages-404-jsx": preferDefault(require("/Users/stephen/Development/Projects/team-8/src/pages/404.jsx")),
+  "component---src-pages-about-jsx": preferDefault(require("/Users/stephen/Development/Projects/team-8/src/pages/about.jsx")),
+  "component---src-pages-app-jsx": preferDefault(require("/Users/stephen/Development/Projects/team-8/src/pages/App.jsx")),
+  "component---src-pages-appt-jsx": preferDefault(require("/Users/stephen/Development/Projects/team-8/src/pages/Appt.jsx")),
+  "component---src-pages-cal-jsx": preferDefault(require("/Users/stephen/Development/Projects/team-8/src/pages/cal.jsx")),
+  "component---src-pages-child-js": preferDefault(require("/Users/stephen/Development/Projects/team-8/src/pages/child.js")),
+  "component---src-pages-contact-jsx": preferDefault(require("/Users/stephen/Development/Projects/team-8/src/pages/contact.jsx")),
+  "component---src-pages-dash-jsx": preferDefault(require("/Users/stephen/Development/Projects/team-8/src/pages/dash.jsx")),
+  "component---src-pages-firebase-js": preferDefault(require("/Users/stephen/Development/Projects/team-8/src/pages/firebase.js")),
+  "component---src-pages-index-jsx": preferDefault(require("/Users/stephen/Development/Projects/team-8/src/pages/index.jsx")),
+  "component---src-pages-log-in-jsx": preferDefault(require("/Users/stephen/Development/Projects/team-8/src/pages/LogIn.jsx")),
+  "component---src-pages-stories-jsx": preferDefault(require("/Users/stephen/Development/Projects/team-8/src/pages/stories.jsx"))
+}
+
