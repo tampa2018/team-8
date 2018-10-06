fa04755a008cfd3717667e026d375a8c00d550c4
Stephen Mylabathula <stephen@Stephens-MacBook-Pro-2.local>
2018-10-06 01:24:42 -0400

    more fields and validation

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
         firstname: '',
         lastname: '',
         email: '',
+        password: '',
         city: '',
         zipcode: '',
         phone: '',
@@ -31,6 +32,7 @@ class App extends Component {
         firstname: this.state.firstname,
         lastname: this.state.lastname,
         email: this.state.email,
+        password: this.state.password,
         city: this.state.city,
         zipcode: this.state.zipcode,
         phone: this.state.phone,
@@ -38,13 +40,14 @@ class App extends Component {
     }
     itemsRef.push(item);
     this.setState({
-      firstname: '',
-      lastname: '',
-      email: '',
-      city: '',
-      zipcode: '',
-      phone: '',
-      focus: ''
+          firstname: '',
+          lastname: '',
+          email: '',
+          password: '',
+          city: '',
+          zipcode: '',
+          phone: '',
+          focus: ''
     });
   }
   componentDidMount() {
@@ -53,11 +56,13 @@ class App extends Component {
       let items = snapshot.val();
       let newState = [];
       for (let item in items) {
+                alert(items[item].firstname);
         newState.push({
           id: item,
           firstname: items[item].firstname,
           lastname: items[item].lastname,
           email: items[item].email,
+          password: items[item].password,
           city: items[item].city,
           zipcode: items[item].zipcode,
           phone: items[item].phone,
@@ -80,23 +85,24 @@ class App extends Component {
         </header>
         <div className='container'>
           <section className='add-item'>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" name="firstname" placeholder="First Name" onChange={this.handleChange} value={this.state.firstname} />
-                    <input type="text" name="lastname" placeholder="Last Name" onChange={this.handleChange} value={this.state.lastname} />
-                    <input type="text" name="email" placeholder="Email" onChange={this.handleChange} value={this.state.email} />
-                    <input type="text" name="city" placeholder="City" onChange={this.handleChange} value={this.state.city} />
-                    <input type="text" name="zipcode" placeholder="ZIP Code" onChange={this.handleChange} value={this.state.zipcode} />
-                    <input type="text" name="phone" placeholder="Phone Number" onChange={this.handleChange} value={this.state.phone} />
-                    <label> Focus Area </label>
-                    <select name="focus" onChange={this.handleChange} value={this.state.focus}>
-                        <option value="none">N/A</option>
-                        <option value="health">Health</option>
-                        <option value="wealth">Wealth</option>
-                        <option value="politics">Politics</option>
-                        <option value="relationships">Relationships</option>
-                    </select>
-                  <button>Sign Up</button>
-                </form>
+            <form onSubmit={this.handleSubmit}>
+                <input required type="text" name="firstname" placeholder="First Name" onChange={this.handleChange} value={this.state.firstname} />
+                <input required type="text" name="lastname" placeholder="Last Name" onChange={this.handleChange} value={this.state.lastname} />
+                <input required type="email" name="email" placeholder="Email" onChange={this.handleChange} value={this.state.email} />
+                <input required type="password" name="password" placeholder="New Password" onChange={this.handleChange} value={this.state.password} />
+                <input required type="text" name="city" placeholder="City" onChange={this.handleChange} value={this.state.city} />
+                <input required type="text" name="zipcode" placeholder="ZIP Code" onChange={this.handleChange} value={this.state.zipcode} />
+                <input required type="tel" name="phone" placeholder="Phone Number" onChange={this.handleChange} value={this.state.phone} />
+                <label> Focus Area </label>
+                <select name="focus" onChange={this.handleChange} value={this.state.focus}>
+                    <option value="none">N/A</option>
+                    <option value="health">Health</option>
+                    <option value="wealth">Wealth</option>
+                    <option value="politics">Politics</option>
+                    <option value="relationships">Relationships</option>
+                </select>
+            <button>Sign Up</button>
+            </form>
           </section>
         </div>
       </div>
